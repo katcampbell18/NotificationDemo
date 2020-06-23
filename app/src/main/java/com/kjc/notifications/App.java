771a6712d6afce6abb7b5547f8b78bce9c7884c9
Katrina Campbell <ktrncampbell@gmail.com>
2020-06-23 12:33:54 -0400

    notification channel groups

--- app/src/main/java/com/kjc/notifications/App.java
+++ app/src/main/java/com/kjc/notifications/App.java
@@ -3,30 +3,41 @@ package com.kjc.notifications;
 import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.os.Build;
 
-public class App extends Application {
+import static com.kjc.notifications.Constants.*;
 
-    public static final String CHANNEL_1_ID = "channel1";
-    public static final String CHANNEL_2_ID = "channel2";
+public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         createNotificationChannel();
-
     }
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannelGroup group1 = new NotificationChannelGroup(
+                    GROUP_1_ID,
+                    "Group 1"
+            );
+
+            NotificationChannelGroup group2 = new NotificationChannelGroup(
+                    GROUP_2_ID,
+                    "Group 2"
+            );
+
             NotificationChannel channel1 = new NotificationChannel(
                     CHANNEL_1_ID,
                     "Channel 1",
                     NotificationManager.IMPORTANCE_HIGH
             );
             channel1.setDescription("This is Channel 1");
+            channel1.setGroup(GROUP_1_ID);
 
             NotificationChannel channel2 = new NotificationChannel(
                     CHANNEL_2_ID,
@@ -34,10 +45,31 @@ public class App extends Application {
                     NotificationManager.IMPORTANCE_LOW
             );
             channel2.setDescription("This is Channel 2");
+            channel2.setGroup(GROUP_2_ID);
+
+            NotificationChannel channel3 = new NotificationChannel(
+                    CHANNEL_3_ID,
+                    "Channel 3",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel3.setDescription("This is Channel 3");
+            channel3.setGroup(GROUP_2_ID);
+
+            NotificationChannel channel4 = new NotificationChannel(
+                    CHANNEL_4_ID,
+                    "Channel 4",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel4.setDescription("This is Channel 4");
+
 
             NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannelGroup(group1);
+            manager.createNotificationChannelGroup(group2);
             manager.createNotificationChannel(channel1);
             manager.createNotificationChannel(channel2);
+            manager.createNotificationChannel(channel3);
+            manager.createNotificationChannel(channel4);
         }
     }
 }
---
+++ app/src/main/java/com/kjc/notifications/Constants.java
@@ -0,0 +1,11 @@
+package com.kjc.notifications;
+
+public class Constants {
+
+    public static final String GROUP_1_ID = "group1";
+    public static final String GROUP_2_ID = "group2";
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_3_ID = "channel3";
+    public static final String CHANNEL_4_ID = "channel4";
+}
