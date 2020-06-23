dc8f47523a793b51f83c472f5fb65d8302c350d2
Katrina Campbell <ktrncampbell@gmail.com>
2020-06-23 11:15:33 -0400

    progress bar notification

--- app/src/main/java/com/kjc/notifications/MainActivity.java
+++ app/src/main/java/com/kjc/notifications/MainActivity.java
@@ -16,6 +16,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
 import android.widget.EditText;
@@ -32,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText titleEditText;
     private EditText messageEditText;
 
-    private MediaSessionCompat mediaSession;
-
     static List<Message> MESSAGES = new ArrayList<>();
 
     @Override
@@ -45,12 +44,9 @@ public class MainActivity extends AppCompatActivity {
         titleEditText = findViewById(R.id.title_edittext);
         messageEditText = findViewById(R.id.message_edittext);
 
-        mediaSession = new MediaSessionCompat(this, "tag");
-
         MESSAGES.add(new Message("Good morning!", "Doris"));
         MESSAGES.add(new Message("Hello!", null));
         MESSAGES.add(new Message("Hi!", "Boris"));
-
     }
 
     public void sendOnChannel1(View v) {
@@ -70,11 +66,11 @@ public class MainActivity extends AppCompatActivity {
         Intent replyIntent;
         PendingIntent replyPendingIntent = null;
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             replyIntent = new Intent(context, DirectReplyReceiver.class);
             replyPendingIntent = PendingIntent.getBroadcast(context,
                     0, replyIntent, 0);
-        }else{
+        } else {
             //start chat activity instead (PendingIntent.getActivity)
             //cancel notification with notificationManagerCompat.cancel(id)
         }
@@ -113,28 +109,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendOnChannel2(View view) {
-        String title = titleEditText.getText().toString();
-        String message = messageEditText.getText().toString();
+        final int progressMax = 100;
 
-        Bitmap artwork = BitmapFactory.decodeResource(getResources(), R.drawable.ic_bookworm);
-
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+        final NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_two)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setLargeIcon(artwork)
-                .addAction(R.drawable.ic_dislike, "Dislike", null)
-                .addAction(R.drawable.ic_previous, "Previous", null)
-                .addAction(R.drawable.ic_pause, "Pause", null)
-                .addAction(R.drawable.ic_next, "Next", null)
-                .addAction(R.drawable.ic_like, "Like", null)
-                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
-                        .setShowActionsInCompactView(1, 2, 3)
-                        .setMediaSession(mediaSession.getSessionToken()))
-                .setSubText("Sub Text")
+                .setContentTitle("Download")
+                .setContentText("Download in progress")
                 .setPriority(NotificationCompat.PRIORITY_LOW)
-                .build();
+                .setOngoing(true)
+                .setOnlyAlertOnce(true)
+                .setProgress(progressMax, 0, false);
 
-        notificationManager.notify(2, notification);
+        notificationManager.notify(2, notification.build());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(2000);
+                for (int progress = 0; progress <= progressMax; progress += 20) {
+//                    notification.setProgress(progressMax, progress, false);
+//                    notificationManager.notify(2, notification.build());
+                    SystemClock.sleep(1000);
+                }
+                notification.setContentText("Downlaod finished.")
+                        .setProgress(0, 0, false)
+                        .setOngoing(false);
+                notificationManager.notify(2, notification.build());
+            }
+        }).start();
     }
 }
